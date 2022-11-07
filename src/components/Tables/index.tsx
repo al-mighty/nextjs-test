5c66b23908ce676b9adf8dd7c6b62a939036d54a
Kovalev Vyacheslav <dev.vyacheslav@gmail.com>
2022-11-07 10:24:50 +0300

    fix

--- src/components/Tables/index.tsx
+++ src/components/Tables/index.tsx
@@ -2,7 +2,7 @@ import {Table} from 'antd';
 import React, {useMemo} from 'react';
 import s from 'src/components/Tables/table.module.scss';
 
-const MedTable = ({columns, data,type}: { columns: any[], data: any[],type?:string }):any => {
+const MedTable = ({columns, data,type}: { columns: any[], data: any[],type?:string }) => {
     const myColumns = useMemo(() => columns, [columns])
     const myData = useMemo(() => data, [data])
     return <Table className={`${s.customTable} ${type==='full'?s.fullTable:''}`} pagination={false} columns={myColumns} dataSource={myData}/>;
